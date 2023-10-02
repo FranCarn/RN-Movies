f662abbf335e509d7593e28e75e935680d77a4fb
@FranCarn <carnevalefranco99@gmail.com>
2023-10-01 23:37:21 -0300

    initial data interface ready

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -4,7 +4,7 @@ const movieDB = axios.create({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
     api_key: process.env.api_key,
-    languaje: 'en-US',
+    language: 'en-US',
   },
 });
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 import movieDB from '../api/movieDB';
+import {MovieDBNowPlaying} from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(res => {
-      console.log(res.data);
+    movieDB.get<MovieDBNowPlaying>('/now_playing').then(res => {
+      console.log(res.data.results);
     });
   }, []);
 
