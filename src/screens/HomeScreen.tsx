97b9267bf4a2bb7f9706ed5362ff05f83fce1b13
@FranCarn <carnevalefranco99@gmail.com>
2023-10-04 18:36:01 -0300

    movie flat list ready

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Carousel from 'react-native-snap-carousel';
 
-import {View, ActivityIndicator, Dimensions} from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  ScrollView,
+  Text,
+  View,
+} from 'react-native';
 import {useMovies} from '../hooks/useMovies';
 import {MoviePoster} from '../components/MoviePoster';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
@@ -18,15 +25,29 @@ export const HomeScreen = () => {
       </View>
     );
   return (
-    <View style={{marginTop: top + 10}}>
-      <View style={{height: 440}}>
-        <Carousel
-          data={moviesInTheatre}
-          renderItem={({item}) => <MoviePoster movie={item} />}
-          sliderWidth={windowWidth}
-          itemWidth={300}
-        />
+    <ScrollView>
+      <View style={{marginTop: top + 10}}>
+        <View style={{height: 440}}>
+          <Carousel
+            data={moviesInTheatre}
+            itemWidth={300}
+            renderItem={({item}) => <MoviePoster movie={item} />}
+            sliderWidth={windowWidth}
+          />
+        </View>
+        <View style={{height: 260}}>
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>Popular</Text>
+          <FlatList
+            data={moviesInTheatre}
+            horizontal
+            keyExtractor={item => item.id.toString()}
+            renderItem={({item}) => (
+              <MoviePoster movie={item} width={140} height={200} />
+            )}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
