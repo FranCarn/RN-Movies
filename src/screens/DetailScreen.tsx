40b2363c14521a7822169cc3ce5abe95ee097722
@FranCarn <carnevalefranco99@gmail.com>
2023-10-08 02:06:11 -0300

    detail page initial design

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,16 +1,54 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
 import {StackScreenProps} from '@react-navigation/stack';
 import {RootStackParams} from '../navigation/Navigation';
+import {ScrollView} from 'react-native-gesture-handler';
+
+const screenHeight = Dimensions.get('screen').height;
 
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> {}
 
 export const DetailScreen = ({navigation, route}: Props) => {
   const movie = route.params;
-
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
   return (
-    <View>
-      <Text>DetailScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.imageContainer}>
+        <Image source={{uri}} style={styles.image} />
+      </View>
+      <View style={styles.marginContainer}>
+        <Text style={styles.subTitle}>{movie.original_title}</Text>
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+  },
+  imageContainer: {
+    overflow: 'hidden',
+    width: '100%',
+    height: screenHeight * 0.7,
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+    flex: 1,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+
+    elevation: 3,
+  },
+  marginContainer: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  subTitle: {fontSize: 16, opacity: 0.8},
+  title: {fontSize: 20, fontWeight: 'bold'},
+});
